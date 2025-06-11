00a795a080bf7a19323b9ce8369f3c56eb89a786
felipemartinsbnf <felipemartinsbn@gmail.com>
2025-06-11 01:48:17 -0300

    feat: criando os testes

--- Tests/configs.py
+++ Tests/configs.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.by import By
 
 def Logar(driver):
     driver.get("https://extensao-first-step.vercel.app/")
-    driver.set_window_size(1176, 702)
+    #driver.get("http://localhost:5137/")
+    driver.set_window_size(1920, 1080)
     driver.find_element(By.CSS_SELECTOR, "a:nth-child(2) > .undefined").click()
     driver.find_element(By.CSS_SELECTOR, ".input-card-div:nth-child(1) > .input-card-input").click()
     driver.find_element(By.CSS_SELECTOR, ".input-card-div:nth-child(1) > .input-card-input").send_keys("felipemartinsbn@gmail.com")
--- Tests/test_login.py
+++ Tests/test_login.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from Tests.configs import Logar
 
+# Teste para verificar se é possível logar e cair na página de dashboard
 
 class TestLogin():
   def setup_method(self, method):
@@ -14,5 +18,10 @@ class TestLogin():
   def test_login(self):
     driver = Logar(self.driver)
     driver.find_element(By.CSS_SELECTOR, ".btn-header:nth-child(3) > .btn").click()
+
+    WebDriverWait(driver, 10).until(
+        EC.url_contains("dash")
+    )
+
     assert "dash" in driver.current_url
   
